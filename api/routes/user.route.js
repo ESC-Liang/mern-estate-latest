a463cd25ea4af48e0e1dbcb5aa53fb4fc6526887
ESC-Liang <bushliang2003@hotmail.com>
2024-08-16 18:27:55 +1000

    create userupdate api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
 const router = express.Router();
+import { updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyuser.js";
 
 router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
