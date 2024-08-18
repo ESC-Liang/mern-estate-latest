2d9c1f7f31e52cdf53eb7069547802d915ecf75b
ESC-Liang <bushliang2003@hotmail.com>
2024-08-19 00:05:31 +1000

    create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 
 mongoose
   .connect(
