2b236f27acb851d3461d712005157bc8b7ac2421
ESC-Liang <bushliang2003@hotmail.com>
2024-08-21 21:00:08 +1000

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,5 +7,6 @@ import { verifyToken } from "../utils/verifyuser.js";
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListing);
 
 export default router;
